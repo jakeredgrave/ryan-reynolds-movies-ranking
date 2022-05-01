e22b64106a4121f699a08094499f3e988280d20b
Jakub Staniek <jakub.staniek@icloud.com>
2022-05-01 16:45:34 +0200

    Adding page header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Ranking from "./Components/Ranking";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello There!</h1>
+      <div className="header">
+        <div className="container flex">
+          <h1>Ryan Reynolds' Movies Ranking</h1>
+        </div>
+      </div>
+
+      <Ranking />
     </div>
   );
 }
---
+++ src/Components/Ranking.js
@@ -0,0 +1,5 @@
+const Ranking = () => {
+  return <div className="ranking-box card"></div>;
+};
+
+export default Ranking;
